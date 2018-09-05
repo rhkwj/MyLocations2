402c1e83c48d60aff13f82086c2d77b8be339163
rhkwj <rhkwj@naver.com>
2018-09-05 09:42:57 +0800

    Initial Commit

---
+++ MyLocations2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyLocations2
+//
+//  Created by Kim Yeon Jeong on 2018/9/5.
+//  Copyright © 2018 Kim Yeon Jeong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
