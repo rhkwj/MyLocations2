9af7610fbd01f02e5da12e73b3ecb0848ae2af77
rhkwj <rhkwj@naver.com>
2018-09-06 13:51:08 +0800

    page 579 and stuck

--- MyLocations2/LocationDetailsViewController.swift
+++ MyLocations2/LocationDetailsViewController.swift
@@ -10,7 +10,46 @@ import Foundation
 import UIKit
 import CoreLocation
 
+private let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .short
+    return formatter
+}()
+
+
 class LocationDetailsViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        descriptionTextView.text = ""
+        categoryLabel.text = ""
+        latitudeLabel.text = String(format: "%.8f",coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f",coordinate.longitude)
+        if let placemark = placemark {
+            addressLabel.text = string(from: placemark)
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+        dateLabel.text = format(date: Date())
+    }
+   
+    
+    // MARK:- Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TagLocation" {
+    let controller = segue.destination as! LocationDetailsViewController
+    controller.coordinate = location!.coordinate
+    controller.placemark = placemark
+      }
+    }
+
+    
+    
+    func format(date: Date) -> String {
+        return dateFormatter.string(from: date)
+    }
+    
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
@@ -28,3 +67,7 @@ class LocationDetailsViewController: UITableViewController {
 
 var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
 var placemark: CLPlacemark?
+
+
+
+
