3bfb1a6d4df2de277bf5e9c91a71adb3a9ca6784
rhkwj <rhkwj@naver.com>
2018-09-12 08:16:46 +0800

    page 637

--- MyLocations2/AppDelegate.swift
+++ MyLocations2/AppDelegate.swift
@@ -30,6 +30,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        print(applicationDocumentsDirectory)
+        
         // Override point for customization after application launch.
         let tabController = window!.rootViewController as! UITabBarController
         if let tabViewControllers = tabController.viewControllers {
