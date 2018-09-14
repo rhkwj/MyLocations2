6172b0e27e43b734f5774c5b69c1871a9a6824a5
rhkwj <rhkwj@naver.com>
2018-09-14 08:09:44 +0800

    page 644

--- MyLocations2/Functions.swift
+++ MyLocations2/Functions.swift
@@ -13,6 +13,10 @@ let applicationDocumentsDirectory: URL = {
     return paths[0]
 }()
 
+let CoreDataSaveFailedNotification = Notification.Name(rawValue: "CoreDataSaveFailedNotification")
+func fatalCoreDataError(_ error: Error) { print("*** Fatal error: \(error)"); NotificationCenter.default.post( name: CoreDataSaveFailedNotification, object : nil)
+}
+
 func afterDelay(_ seconds: Double, run: @escaping () -> Void) {
     
     DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: run)
