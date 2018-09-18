ccc18b16da702500dc11a5fd0338cf563433e4ff
rhkwj <rhkwj@naver.com>
2018-09-18 15:46:40 +0800

    page 659

--- MyLocations2/LocationCellTableViewController.swift
+++ MyLocations2/LocationCellTableViewController.swift
@@ -20,8 +20,35 @@ class LocationCellTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    
+    
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
+    
+    func configure(for location: Location) {
+        if location.locationDescription.isEmpty {
+            descriptionLabel.text = "(No Description)"
+        }
+        else {  descriptionLabel.text = location.locationDescription
+        }
+        if let placemark = location.placemark {
+            var text = ""
+            if let s = placemark.subThoroughfare {
+                text += s + " " }
+            if let s = placemark.thoroughfare {
+                text += s + ", "
+            }
+            if let s = placemark.locality {
+                text += s }
+            addressLabel.text = text
+        } else {
+            addressLabel.text = String(format:
+                "Lat: %.8f, Long: %.8f",location.latitude, location.longitude)
+        }
+
+    }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
