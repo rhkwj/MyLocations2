34e4bf70a9e6746fcb62f69fef9dbb69ca310658
rhkwj <rhkwj@naver.com>
2018-09-26 21:59:52 +0800

    page 733 and of  capter 30

---
+++ MyLocations2/UIImage+Resize.swift
@@ -0,0 +1,25 @@
+//
+//  UIImage+Resize.swift
+//  MyLocations2
+//
+//  Created by Kim Yeon Jeong on 2018/9/26.
+//  Copyright © 2018 Kim Yeon Jeong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    func resized(withBounds bounds: CGSize) -> UIImage {
+        let horizontalRatio = bounds.width / size.width
+        let verticalRatio = bounds.height / size.height
+        let ratio = min(horizontalRatio, verticalRatio)
+        let newSize = CGSize(width: size.width * ratio,
+                             height: size.height * ratio)
+        UIGraphicsBeginImageContextWithOptions(newSize, true, 0)
+        draw(in: CGRect(origin: CGPoint.zero, size: newSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+}
